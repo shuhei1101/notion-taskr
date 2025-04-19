6364641c44fac8eb980eb468a490a81bb85d295c
shuhei <shuhei.nishikawa_111@icloud.com>
2025-04-19 15:18:56 +0000

    utilに新規メソッド[truncate_decimal]を追加

--- src/util/converter.py
+++ src/util/converter.py
@@ -6,5 +6,8 @@ def man_hour_to_man_days(man_hour: float) -> float:
     return man_hour / config.MAN_HOUR_PER_DAY
 
 def truncate_decimal(num: float) -> str:
-    '''小数点以下が0の場合は整数部分のみ表示'''
+    '''小数点以下を1桁に切り捨てる
+    
+    ただし、小数点以下が0の場合は整数部分のみ表示
+    '''
     return f'{num:.1f}'.rstrip('0').rstrip('.')
