2f9a0c8e45e13c1eaea6a85cae5f604caa05de68
shuhei <shuhei.nishikawa_111@icloud.com>
2025-04-25 14:42:19 +0000

    タスクサービスクラス（新規作成）

--- src/domain/task_service.py
+++ src/domain/task_service.py
@@ -1,3 +1,7 @@
+import copy
+from domain.excuted_task import ExcutedTask
+from domain.scheduled_task import ScheduledTask
+from domain.name_labels.man_days_label import ManDaysLabel
 from domain.task import Task
 
 
@@ -5,8 +9,12 @@ class TaskService:
     def get_changed_tasks(self, tasks: list[Task]):
         '''変更されたタスクを取得する'''
         changed_tasks = list(filter(
-            lambda task: task.is_changed,
+            lambda task: task.is_updated,
             tasks
         ))
         
         return changed_tasks
+
+
+            
+                
