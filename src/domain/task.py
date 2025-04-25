b2d14ddf511445cc3af9a10c21e1425c369d0995
shuhei <shuhei.nishikawa_111@icloud.com>
2025-04-25 14:41:47 +0000

    タスクモデルクラス（is_updated変数の追加／新規メソッドの追加）

--- src/domain/task.py
+++ src/domain/task.py
@@ -1,16 +1,38 @@
 from dataclasses import dataclass
-from typing import List
+from typing import TYPE_CHECKING, List
 
 from domain.task_name import TaskName
 
+if TYPE_CHECKING:
+    from domain.name_labels.id_label import IdLabel
+    from domain.name_labels.man_days_label import ManDaysLabel
+
 @dataclass
-class Task():
+class Task:
     '''タスクモデル'''
     page_id: str
     name: TaskName
     tags: List[str]
-    is_changed: bool = False
+    is_updated: bool = False
     
+    def update_man_days_label(self, man_days_label: 'ManDaysLabel'):
+        '''工数ラベルを登録するメソッド'''
 
-    
+        self.is_updated = True
+        self.name.man_days_label = man_days_label
+
+    def update_id_label(self, label: 'IdLabel'):
+        '''IDラベルを登録するメソッド
+
+        :param IdLabel label: IDラベル
+        '''
+        self.is_updated = True
+        self.name.id_label = label    
         
+    def get_display_name(self) -> str:
+        '''表示用のタスク名を取得するメソッド
+
+        :return: 表示用のタスク名
+        '''
+        return str(self.name)
+    
