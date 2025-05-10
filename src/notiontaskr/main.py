d5cf44f7021513d6abd393c697153c7a62d0b2ac
shuhei <shuhei.nishikawa_111@icloud.com>
2025-05-11 05:29:44 +0900

    gunicornコマンドの修正

--- src/notiontaskr/main.py
+++ src/notiontaskr/main.py
@@ -1,20 +1,24 @@
 from flask import Flask, render_template
-import logging
 
 app = Flask(__name__)
 
-logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/run-deployment-tasks")
 def update_executed_task_id():
-    '''デプロイ時に実行するタスクを実行する'''
+    """デプロイ時に実行するタスクを実行する"""
     from notiontaskr.job import run_deployment_tasks
+
     run_deployment_tasks()
     return "Deployment tasks executed successfully!"
 
+
 if __name__ == "__main__":
+    # update_executed_task_id()
     app.run(host="0.0.0.0", port=5000, debug=True)
