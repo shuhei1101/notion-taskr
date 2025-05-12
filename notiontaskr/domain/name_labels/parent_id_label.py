3921aaf0a14b1e3ee3f63585b658a9d3eb3baf73
shuhei <shuhei.nishikawa_111@icloud.com>
2025-05-12 21:01:23 +0900

    更新

--- notiontaskr/domain/name_labels/parent_id_label.py
+++ notiontaskr/domain/name_labels/parent_id_label.py
@@ -4,12 +4,13 @@ from notiontaskr.domain.name_labels.label_registerable import LabelRegisterable
 from notiontaskr.domain.name_labels.name_label import NameLabel
 from notiontaskr.domain.value_objects.notion_id import NotionId
 
+
 @dataclass
 class ParentIdLabel(NameLabel):
-    
+
     @classmethod
-    def from_property(cls, parent_id: NotionId) -> 'ParentIdLabel':
-        '''親IDラベルを生成する'''
+    def from_property(cls, parent_id: NotionId) -> "ParentIdLabel":
+        """親IDラベルを生成する"""
 
         return cls(
             key="親",
@@ -17,18 +18,21 @@ class ParentIdLabel(NameLabel):
         )
 
     @classmethod
-    def parse_and_register(cls, key: str, value: str, delegate: 'LabelRegisterable') -> 'ParentIdLabel':
-        '''ラベルを解析して登録する'''
+    def parse_and_register(
+        cls, key: str, value: str, delegate: "LabelRegisterable"
+    ) -> "ParentIdLabel":
+        """ラベルを解析して登録する"""
         if not key == "親":
-            raise ValueError(f'Unknown key: {key}')
+            raise ValueError(f"Unknown key: {key}")
 
-        delegate.register_parent_id_label(cls(
-            key=key,
-            value=str(value),
-        ))
+        delegate.register_parent_id_label(
+            cls(
+                key=key,
+                value=str(value),
+            )
+        )
 
     def __eq__(self, other):
         if not isinstance(other, ParentIdLabel):
             return NotImplemented
         return self.value == other.value
-    
