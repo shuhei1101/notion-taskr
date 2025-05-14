29cb6dd8f87a05acdfb19433402cda0b15521a47
shuhei <shuhei.nishikawa_111@icloud.com>
2025-05-14 17:15:53 +0900

    パッケージ名変更

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="notion-api",  # パッケージ名
+    name="notiontaskr",  # パッケージ名
     version="0.1",  # バージョン番号（公開しない場合は削除可能）
     packages=find_packages(exclude=["tests"]),
     test_suite="tests",  # テストスイートの指定
