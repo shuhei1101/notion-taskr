cf8cdfcdf4ec46f660a95d5770fca752b07066a6
shuhei <shuhei.nishikawa_111@icloud.com>
2025-05-15 17:48:34 +0900

    コーディングルールの適用

--- notiontaskr/domain/name_labels/man_hours_label.py
+++ notiontaskr/domain/name_labels/man_hours_label.py
@@ -14,35 +14,40 @@ from notiontaskr.domain.name_labels.name_label import NameLabel
 
 @dataclass
 class ManHoursLabel(NameLabel):
-    '''工数のラベルクラス'''
+    """工数のラベルクラス"""
 
     @classmethod
-    def from_man_hours(cls, scheduled_man_hours: ManHours, executed_man_hours: ManHours) -> str:
-        '''工数タグを生成する'''
+    def from_man_hours(
+        cls, scheduled_man_hours: ManHours, executed_man_hours: ManHours
+    ) -> "ManHoursLabel":
+        """工数タグを生成する"""
 
         return cls(
             key=config.MAN_HOURS_EMOJI,
-            value=f'{truncate_decimal(executed_man_hours.value)}/{truncate_decimal(scheduled_man_hours.value)}',
+            value=f"{truncate_decimal(executed_man_hours.value)}/{truncate_decimal(scheduled_man_hours.value)}",
         )
-    
+
     @classmethod
-    def parse_and_register(cls, key: str, value: str, delegate: 'LabelRegisterable'):
-        '''ラベルを解析して登録する'''
+    def parse_and_register(cls, key: str, value: str, delegate: "LabelRegisterable"):
+        """ラベルを解析して登録する"""
         if not is_emoji_matches(key, config.MAN_HOURS_EMOJI):
-            raise ValueError(f'Unknown key: {key}')
-        
-        delegate.register_man_hours_label(cls(
-            key=key,
-            value=str(value),
-        ))
+            raise ValueError(f"Unknown key: {key}")
+
+        delegate.register_man_hours_label(
+            cls(
+                key=key,
+                value=str(value),
+            )
+        )
 
     def __eq__(self, other):
         if not isinstance(other, ManHoursLabel):
             return False
         return self.value == other.value
-    
+
+
 # 動作確認用
-if __name__ == '__main__':
-    value1=emoji.demojize('⏱')
-    value2=emoji.demojize(config.MAN_HOURS_EMOJI)
+if __name__ == "__main__":
+    value1 = emoji.demojize("⏱")
+    value2 = emoji.demojize(config.MAN_HOURS_EMOJI)
     print(value1 == value2)
