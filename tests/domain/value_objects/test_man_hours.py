932ff9889779828eb80542867de554e1f70bf426
shuhei <shuhei.nishikawa_111@icloud.com>
2025-05-16 00:28:00 +0900

    Test_from_notion_dateメソッドの作成

--- tests/domain/value_objects/test_man_hours.py
+++ tests/domain/value_objects/test_man_hours.py
@@ -1,5 +1,7 @@
 import pytest
 from notiontaskr.domain.value_objects.man_hours import ManHours
+from notiontaskr.domain.value_objects.notion_date import NotionDate
+from datetime import datetime
 
 
 class TestManHours:
@@ -26,3 +28,8 @@ class TestManHours:
         def test_ManHoursをfloatに変換できること(self):
             assert float(ManHours(1)) == 1.0
             assert float(ManHours(1.5)) == 1.5
+
+    class Test_from_notion_dateメソッド:
+        def test_引数がNotionDateのとき正常に初期化されること(self):
+            date = NotionDate(datetime(2023, 1, 1), datetime(2023, 1, 2))
+            assert ManHours.from_notion_date(date).value == 24.0
