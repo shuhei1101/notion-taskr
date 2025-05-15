346ef0648c3c3b839ea110d4d7b82d902f161f44
shuhei <shuhei.nishikawa_111@icloud.com>
2025-05-16 00:49:03 +0900

    pep8に伴う修正

--- notiontaskr/domain/value_objects/notion_id.py
+++ notiontaskr/domain/value_objects/notion_id.py
@@ -3,9 +3,10 @@ from dataclasses import dataclass
 
 @dataclass
 class NotionId:
-    number: str = None
-    prefix: str = None
-    def __init__(self, number: str, prefix: str = None):
+    number: str = ""
+    prefix: str = ""
+
+    def __init__(self, number: str, prefix: str = ""):
         if not number:
             raise ValueError(f"IDのnumberは必須です。")
         self.number = str(number)
@@ -14,7 +15,7 @@ class NotionId:
     def __hash__(self):
         return hash(self.number)
 
-    def __eq__(self, other: 'NotionId'):
+    def __eq__(self, other: "object"):
         if not isinstance(other, NotionId):
             return False
         return self.number == other.number
