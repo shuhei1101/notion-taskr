5bfe4f9235c51731bc0662fdf8baae38cdb0670d
shuhei <shuhei.nishikawa_111@icloud.com>
2025-05-16 00:24:00 +0900

    Test_addメソッドの作成

--- tests/domain/value_objects/test_man_hours.py
+++ tests/domain/value_objects/test_man_hours.py
@@ -13,3 +13,11 @@ class TestManHours:
 
         def test_引数valueがNoneのときデフォルト値0で初期化されること(self):
             assert ManHours(None).value == 0  # type: ignore
+
+    class Test_addメソッド:
+        def test_引数がManHoursのとき正常に加算されること(self):
+            assert ManHours(1) + ManHours(2) == ManHours(3)
+
+        def test_引数がManHoursでないときNotImplementedErrorが発生すること(self):
+            with pytest.raises(NotImplementedError):
+                ManHours(1) + 1  # type: ignore
