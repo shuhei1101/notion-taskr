99b2d0d761ad779795dbbb09b9c719f9d6f0cd83
shuhei <shuhei.nishikawa_111@icloud.com>
2025-05-21 04:31:02 +0900

    動作確認用更新

--- notiontaskr/application/task_application_service.py
+++ notiontaskr/application/task_application_service.py
@@ -442,12 +442,12 @@ class TaskApplicationService:
 
 if __name__ == "__main__":
     service = TaskApplicationService()
-    start, end = dt_to_month_start_end(datetime(year=2024, month=12, day=1))
+    start, end = dt_to_month_start_end(datetime(year=2025, month=5, day=1))
     uptime_data_by_tag = asyncio.run(
         service.get_uptime(
             from_=start,
             to=end,
-            tags=["notion-api", "プライベート"],
+            tags=["notion-api", "お小遣いクエストボード"],
         )
     )
     print(uptime_data_by_tag.to_json())
