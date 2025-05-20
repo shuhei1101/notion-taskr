4e0fde019160f4f32503ee23760ee9d8eb3f00b1
shuhei <shuhei.nishikawa_111@icloud.com>
2025-05-21 04:32:26 +0900

    動作確認用コードの移動

--- notiontaskr/application/task_application_service.py
+++ notiontaskr/application/task_application_service.py
@@ -438,16 +438,3 @@ class TaskApplicationService:
             )
 
         await asyncio.gather(*tasks)
-
-
-if __name__ == "__main__":
-    service = TaskApplicationService()
-    start, end = dt_to_month_start_end(datetime(year=2025, month=5, day=1))
-    uptime_data_by_tag = asyncio.run(
-        service.get_uptime(
-            from_=start,
-            to=end,
-            tags=["notion-api", "お小遣いクエストボード"],
-        )
-    )
-    print(uptime_data_by_tag.to_json())
--- tests/application/test_task_application_service.py
+++ tests/application/test_task_application_service.py
@@ -1,27 +1,19 @@
-# import asyncio
-# from datetime import datetime
-# from notiontaskr.application.task_application_service import TaskApplicationService
-# from notiontaskr.util.converter import dt_to_month_start_end
-# from pytest import fixture
+# 動作確認用
+import asyncio
+from datetime import datetime
+from notiontaskr.application.task_application_service import TaskApplicationService
+
+from notiontaskr.util.converter import dt_to_month_start_end
 
 
-# class Test_get_uptime:
-#     @fixture
-#     def service(self):
-#         # TaskApplicationServiceのインスタンスを作成
-#         return TaskApplicationService()
-
-#     def test_結合試験(self, service: TaskApplicationService):
-#         """タグ`notion-api`の2024/12は1:22の稼働実績がある"""
-#         dt = datetime(year=2024, month=12, day=1)
-#         start, end = dt_to_month_start_end(dt)
-#         uptime_data_by_tag = asyncio.run(
-#             service.get_uptime(
-#                 from_=start,
-#                 to=end,
-#                 tags=["notion-api"],
-#             )
-#         )
-#         data = uptime_data_by_tag.get_data("notion-api")
-#         assert data.tag == "notion-api"
-#         assert data.uptime == 1.3666666666666667
+if __name__ == "__main__":
+    service = TaskApplicationService()
+    start, end = dt_to_month_start_end(datetime(year=2025, month=5, day=1))
+    uptime_data_by_tag = asyncio.run(
+        service.get_uptime(
+            from_=start,
+            to=end,
+            tags=["notion-api", "お小遣いクエストボード"],
+        )
+    )
+    print(uptime_data_by_tag.to_json())
