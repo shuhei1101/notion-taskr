b345f80acc5a6befa00d25908487676fcb7fa4d2
shuhei <shuhei.nishikawa_111@icloud.com>
2025-05-23 03:05:35 +0900

    refactor(ExecutedTasks): メンバ命名変更`tasks`→`_tasks`

--- notiontaskr/domain/executed_tasks.py
+++ notiontaskr/domain/executed_tasks.py
@@ -14,41 +14,41 @@ class ExecutedTasks:
     実績タスクを管理するクラス
     """
 
-    tasks: List[ExecutedTask]
+    _tasks: List[ExecutedTask]
 
     @classmethod
     def from_empty(cls):
-        return cls(tasks=[])
+        return cls(_tasks=[])
 
     @classmethod
     def from_tasks(cls, tasks: List[ExecutedTask]):
         return cls(
-            tasks=tasks,
+            _tasks=tasks,
         )
 
     def append(self, task: ExecutedTask):
         """実績タスクを追加する"""
 
-        self.tasks.append(task)
+        self._tasks.append(task)
 
     def extend(self, tasks: "ExecutedTasks"):
         """実績タスクを追加する"""
 
-        self.tasks.extend(tasks.tasks)
+        self._tasks.extend(tasks._tasks)
 
     def __len__(self):
-        return len(self.tasks)
+        return len(self._tasks)
 
     def get_tasks_by_id(self):
-        return {task.id: task for task in self.tasks}
+        return {task.id: task for task in self._tasks}
 
     def get_tasks_by_page_id(self):
-        return {task.page_id: task for task in self.tasks}
+        return {task.page_id: task for task in self._tasks}
 
     def get_tasks_by_tag(self, tags: "Tags") -> dict[Tag, "ExecutedTasks"]:
         """指定したタグを持つ実績タスクを取得する"""
         executed_tasks_by_tags = {tag: ExecutedTasks.from_empty() for tag in tags}
-        for task in self.tasks:
+        for task in self._tasks:
             for task_tag in task.tags:
                 if task_tag in tags:
                     executed_tasks_by_tags[task_tag].append(task)
