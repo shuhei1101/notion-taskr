29a43ff25104cf9be47c122196cdf1eedea22789
shuhei <shuhei.nishikawa_111@icloud.com>
2025-05-29 12:09:36 +0900

    fix(domain): :recycle: 不要なメソッドの削除(`ExecutedTaskService`)

--- notiontaskr/domain/executed_task_service.py
+++ notiontaskr/domain/executed_task_service.py
@@ -1,5 +1,4 @@
 from notiontaskr.domain.executed_task import ExecutedTask
-from notiontaskr.domain.task_service import TaskService
 from notiontaskr.domain.executed_tasks import ExecutedTasks
 
 from notiontaskr.domain.value_objects.man_hours import ManHours
@@ -9,9 +8,8 @@ from notiontaskr.domain.scheduled_tasks import ScheduledTasks
 
 class ExecutedTaskService:
 
-    @staticmethod
-    def get_tasks_add_id_tag(
-        to: ExecutedTasks, source: ScheduledTasks
+    def get_scheduled_tasks_added_executed_id(
+        self, to: ExecutedTasks, source: ScheduledTasks
     ) -> ScheduledTasks:
         """予定タスクと同じ名前を持つ実績タスクに同じIDを付与し、
         新たにIDが付与された予定タスクのみを返却する"""
@@ -27,13 +25,4 @@ class ExecutedTaskService:
                     break
 
         # 更新した予定タスクを取得する(重複除去)
-        return ScheduledTasks.from_tasks_by_id(updated_tasks.get_tasks_by_id())
-
-    @staticmethod
-    def get_total_man_hours(executed_tasks: list[ExecutedTask]) -> float:
-        """実績タスクの工数を合計する"""
-        total_man_hours = ManHours(0)
-        for task in executed_tasks:
-            total_man_hours += task.man_hours
-
-        return float(total_man_hours)
+        return updated_tasks.get_unique_tasks_by_id()
--- tests/domain/test_executed_task_service.py
+++ tests/domain/test_executed_task_service.py
@@ -1,9 +1,6 @@
 from unittest.mock import Mock
-
 from notiontaskr.domain.executed_task_service import ExecutedTaskService
-
 from notiontaskr.domain.executed_tasks import ExecutedTasks
-
 from notiontaskr.domain.scheduled_tasks import ScheduledTasks
 
 
@@ -20,9 +17,9 @@ class TestExecutedTaskService:
             scheduled_task.name.id_label = "ID1"
             scheduled_task.id = "scheduled_id_1"
             to = ExecutedTasks.from_tasks([executed_task])
-            source = [scheduled_task]
+            source = ScheduledTasks.from_tasks([scheduled_task])
 
-            _ = ExecutedTaskService.get_tasks_add_id_tag(to, source)  # type: ignore
+            _ = ExecutedTaskService().get_scheduled_tasks_added_executed_id(to, source)  # type: ignore
             # `IDを付与するメソッド`と`予定タスクIDを更新するメソッド`が呼び出されることを確認
             executed_task.update_id_label.assert_called_once_with("ID1")
             executed_task.update_scheduled_task_id.assert_called_once_with(
@@ -40,7 +37,7 @@ class TestExecutedTaskService:
             to = ExecutedTasks.from_tasks([executed_task])
             source = [scheduled_task]
 
-            updated_tasks = ExecutedTaskService.get_tasks_add_id_tag(to, source)  # type: ignore
+            updated_tasks = ExecutedTaskService().get_scheduled_tasks_added_executed_id(to, source)  # type: ignore
 
             assert scheduled_task.id == updated_tasks[0].id
 
@@ -51,7 +48,7 @@ class TestExecutedTaskService:
             to = ExecutedTasks.from_tasks([executed_task])
             source = [scheduled_task]
 
-            _ = ExecutedTaskService.get_tasks_add_id_tag(to, source)  # type: ignore
+            _ = ExecutedTaskService().get_scheduled_tasks_added_executed_id(to, source)  # type: ignore
             # 内部のメソッドが呼び出されないことを確認
             executed_task.update_id_label.assert_not_called()
             executed_task.update_scheduled_task_id.assert_not_called()
@@ -68,16 +65,6 @@ class TestExecutedTaskService:
             to = ExecutedTasks.from_tasks([executed_task])
             source = [scheduled_task]
 
-            updated_tasks = ExecutedTaskService.get_tasks_add_id_tag(to, source)  # type: ignore
+            updated_tasks = ExecutedTaskService().get_scheduled_tasks_added_executed_id(to, source)  # type: ignore
             # 新たにIDが付与された予定タスクがない場合は空のリストを返すことを確認
             assert updated_tasks == ScheduledTasks.from_empty()
-
-    class Test_get_total_man_hours:
-        def test_実績タスクの工数を合計できること(self):
-            from notiontaskr.domain.value_objects.man_hours import ManHours
-
-            executed_task1 = Mock(man_hours=ManHours(5))
-            executed_tasks = [executed_task1]
-            result = ExecutedTaskService.get_total_man_hours(executed_tasks)  # type: ignore
-            # 実績タスクの工数を合計できることを確認
-            assert result == 5.0
