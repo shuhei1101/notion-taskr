eb961062d97d00e3c7e36fc51392b5dc47636735
shuhei <shuhei.nishikawa_111@icloud.com>
2025-05-30 16:21:25 +0900

    fix(domain): :bug: メソッド`notify`を非同期に変更(`Notifiable`)

--- notiontaskr/notifier/notifiable.py
+++ notiontaskr/notifier/notifiable.py
@@ -4,5 +4,5 @@ from abc import ABC, abstractmethod
 class Notifiable(ABC):
 
     @abstractmethod
-    def notify(self, message: str) -> None:
+    async def notify(self, message: str) -> None:
         raise NotImplementedError("Subclasses must implement this method.")
